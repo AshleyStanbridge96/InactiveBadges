93823e45099b4e60f14623f78eb1dd0ca97b1df1
Ashley Stanbridge <36490302+AshleyStanbridge96@users.noreply.github.com>
2019-06-27 10:47:43 -0500

    Added comments

--- InactiveBadges - GitHub.sql
+++ InactiveBadges - GitHub.sql
@@ -1,17 +1,22 @@
   WITH BI AS
 (
+--This CTE is joining an employee's card number with their employee number
   SELECT BADGE_C.CARDNO, BADGE_V.BADGE_EMPLOYEE_NO
   FROM BADGE_V 
   INNER join BADGE_C ON BADGE_C.ID = BADGE_V.ID
 ),
 empScans as 
-(select evnt_dat, location, cast(cardno as varchar(5)) as CARDNO, fname, lname, PANEL_DESCRP, CARDSTATUS.DESCRP as statDescrp
+(
+--This CTE is primarily selecting the imporant columns for the report.
+select evnt_dat, location, cast(cardno as varchar(5)) as CARDNO, fname, lname, PANEL_DESCRP, CARDSTATUS.DESCRP as statDescrp
 from EV_LOG
+--This join is getting the cards status i.e. "Active" or "Disabled" and joining it with the card number                                                  
 inner join CARDSTATUS ON CARDSTATUS.STATUS = EV_LOG.STAT_COD
 where (EVNT_DAT is not null)
 and (LNAME is not null)
 group by CARDNO, LOCATION, fname, lname, EVNT_DAT, PANEL_DESCRP, CARDSTATUS.DESCRP
 )
+--This main query is tying everything together
 select * from empScans
 inner join BI on BI.cardno = empScans.CARDNO
 where (evnt_dat >  '20190620 06:00:00')	--Start Date
